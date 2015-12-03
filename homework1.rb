24d8e34f4814000f042c853dd52ee7541ea77a24
Scott Spencer <scottspencer003@yahoo.com>
2015-12-02 19:54:01 -0500

    Finished problems

--- homework1.rb
+++ homework1.rb
@@ -1,5 +1,7 @@
 def full_name(first_name, last_name, title)
-  # Example comment line
+  # This method creates a variable called name and calls for 3 parameters, first_name, last_name and title. 
+  #Then it checks to see which information has been added and assigns that to name. 
+  #The if else statements format the information in the appropriat order.
 
   name = nil
 
@@ -17,3 +19,32 @@ def full_name(first_name, last_name, title)
 
   return name
 end
+
+name_result = full_name("scott", "spencer", "Mr.")
+puts name_result
+
+
+def add(number1, number2)
+  sum = number1 + number2
+  sum
+end  
+
+sum_result = add(10, 31)
+puts sum_result
+
+def join_strings(string1, string2)
+  join = string1 + " " + string2
+  join
+end
+
+join_result = join_strings("Roll", "over")
+puts join_result
+
+
+# the puts method takes the information being called and prints the output on the screen.
+# the return method records the results of the method being run but does not print the result on the screen.  
+
+
+
+
+
